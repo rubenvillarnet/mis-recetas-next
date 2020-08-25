b5132334bd827b0a974d8d001eff9d4105c567d9
Ruben Villar <ruben@rubenvillar.net>
2020-08-25 06:25:10 +0200

    added fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from '../styles/GlobalStyle';
+import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
--- styles/GlobalStyle.js
+++ styles/GlobalStyle.js
@@ -1,6 +1,56 @@
 import { createGlobalStyle } from 'styled-components';
 
+import WalterTurncoat from '../public/fonts/WalterTurncoat-Regular.ttf';
+import QuicksandRegular from '../public/fonts/Quicksand-Regular.ttf';
+import QuicksandBold from '../public/fonts/Quicksand-Bold.ttf';
+import QuicksandLight from '../public/fonts/Quicksand-Light.ttf';
+import QuicksandSemiBold from '../public/fonts/Quicksand-SemiBold.ttf';
+import QuicksandMedium from '../public/fonts/Quicksand-Medium.ttf';
+
 const GlobalStyle = createGlobalStyle`
+
+  @font-face {
+    font-family: "WalterTurncoat";
+    src: url('${WalterTurncoat}');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Quicksand";
+    src: url('${QuicksandRegular}');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Quicksand";
+    src: url('${QuicksandBold}');
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Quicksand";
+    src: url('${QuicksandLight}');
+    font-weight: 300;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Quicksand";
+    src: url('${QuicksandSemiBold}');
+    font-weight: 600;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Quicksand";
+    src: url('${QuicksandMedium}');
+    font-weight: 500;
+    font-style: normal;
+  }
+
   * {
     box-sizing: border-box;
   }
@@ -9,6 +59,11 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+
+  html{
+    font-family: "Quicksand", sans-serif;
+  }
+
   ul {
     padding: 0;
     margin: 0;
@@ -19,6 +74,7 @@ const GlobalStyle = createGlobalStyle`
 
   h1, h2, h3, h4, h5, h6{
     margin: 0;
+    font-family: "WalterTurncoat", sans-serif;
   }
 
   p {
