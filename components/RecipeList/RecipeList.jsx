99757461e457ca117cb9002a05608d7a90cdf9b4
Ruben Villar <ruben@rubenvillar.net>
2020-09-01 23:18:04 +0200

    picture icon in recipe list

--- components/RecipeList/RecipeList.jsx
+++ components/RecipeList/RecipeList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import classNames from 'classnames';
 
 import StyledRecipeList from './RecipeListStyle';
 
@@ -7,11 +8,16 @@ export default function RecipeList({ recipes, type }) {
   return (
     <StyledRecipeList>
       {recipes.map((recipe) => (
-        <li key={recipe.slug}>
-          <Link href={`/${type}/[slug]`} as={`/${type}/${recipe.slug}`}>
-            <a>{recipe.title}</a>
-          </Link>
-        </li>
+        <Link href={`/${type}/[slug]`} as={`/${type}/${recipe.slug}`}>
+          <a>
+            <li
+              key={recipe.slug}
+              className={classNames({ image: !!recipe.image })}
+            >
+              {recipe.title}
+            </li>
+          </a>
+        </Link>
       ))}
     </StyledRecipeList>
   );
--- components/RecipeList/RecipeListStyle.jsx
+++ components/RecipeList/RecipeListStyle.jsx
@@ -1,22 +1,45 @@
 import styled from 'styled-components';
+import imageIcon from '../../public/images/picture.svg';
 
 const StyledRecipeList = styled.ul`
   margin-top: 3rem;
   margin-bottom: 3rem;
   border: 1px solid ${({ theme }) => theme.colors.fadedDark};
   border-radius: 4px;
-  li {
-    padding: 0.75rem 1rem;
-    border-bottom: 1px solid ${({ theme }) => theme.colors.fadedDark};
-    &::not(::last-child) {
-      border-bottom: none;
-    }
-    a {
-      text-transform: uppercase;
-      color: ${({ theme }) => theme.colors.dark};
-      font-size: 1.25rem;
+  a {
+    text-transform: uppercase;
+    color: ${({ theme }) => theme.colors.dark};
+    font-size: 1.25rem;
+    li {
+      padding: 0.75rem 1rem;
+      padding-left: 2.5rem;
+      border-bottom: 1px solid ${({ theme }) => theme.colors.fadedDark};
+      cursor: pointer;
+      transition: all 0.5s;
+      &::not(::last-child) {
+        border-bottom: none;
+      }
       &:hover {
-        text-decoration: underline;
+        background-color: ${({ theme }) => theme.colors.fadedDark};
+        a {
+          color: ${({ theme }) => theme.colors.white};
+        }
+      }
+      &.image {
+        position: relative;
+        &::before {
+          content: '';
+          width: 1rem;
+          height: 1rem;
+          background: url(${imageIcon});
+          background-repeat: no-repeat;
+          position: absolute;
+          left: 1rem;
+          top: 50%;
+          transform: translateY(-50%);
+        }
+      }
+      a {
       }
     }
   }
